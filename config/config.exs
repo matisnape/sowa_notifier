8a5a9c879030d2163232d8680ada2e3d0d666b39
Anna Nowak <matisnape@wp.pl>
2024-10-06 21:32:56 +0200

    Deploy configs

--- config/config.exs
+++ config/config.exs
@@ -4,4 +4,6 @@ import Config
 config :sowa_notifier, SowaScheduler,
   jobs: [
     {"*/2 * * * *", {SowaScheduler, :run, []}}
+    # monday through saturday between 8 and 19 every 10 minutes
+    # {"*/10 8-19 * * 1-6", {SowaScheduler, :run, []}}
   ]
